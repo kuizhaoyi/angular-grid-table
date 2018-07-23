4a2eb814c5d0726485c4bf3c17e08d6406561706
lwq <lwq@1mdata.com>
2018-07-23 16:48:41 +0800

    第一次提交

---
+++ src/enuoGrid.js
@@ -0,0 +1,98 @@
+angular.module('enuo.grid')
+    .directive("enuoGrid", function() {
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: {
+                config: '=',
+                gridSource: '='
+            },
+            templateUrl: "enuo/grid/templates/enuoGrid.html",
+            controller: function($scope, $element, $attrs) {
+                //$scope.selectedList = [];
+                $scope.config.selectedList = $scope.config.selectedList || [];
+                $scope.checkAll = false;
+                $scope.columnsCount = $scope.config.pkid ? $scope.config.columns.length + 1 : $scope.config.columns.length;
+                $scope.contentStyle = $scope.config.style ? $scope.config.style.contentStyle : {};
+
+                $scope.fnItemChecked = function(item) {
+                    var value = item[$scope.config.pkid];
+                    return $scope.config.selectedList.filter(function(data) {
+                        return data[$scope.config.pkid] == value;
+                    }).length > 0;
+                };
+
+                $scope.fnCheckIsAll = function() {
+                    if (!$scope.gridSource) {
+                        return false;
+                    }
+
+                    var checkCount = $scope.gridSource.filter(function(data) {
+                        return $scope.fnItemChecked(data);
+                    }).length;
+                    $scope.checkAll = checkCount == $scope.gridSource.length;
+                }
+
+                $scope.fnCheckItem = function(item) {
+                    var value = item[$scope.config.pkid];
+                    var selecteds = $scope.config.selectedList.filter(function(data) {
+                        return data[$scope.config.pkid] == value;
+                    });
+                    var selected = selecteds.length > 0 ? selecteds[0] : null;
+                    if (selected) {
+                        var index = $scope.config.selectedList.indexOf(selected);
+                        $scope.config.selectedList.splice(index, 1);
+                    } else {
+                        $scope.config.selectedList.push(item);
+                    }
+                    $scope.fnCheckIsAll();
+                };
+
+                $scope.fnCheckAll = function() {
+                    $scope.checkAll = !$scope.checkAll;
+                    angular.forEach($scope.gridSource, function(data, index, array) {
+                        var value = data[$scope.config.pkid];
+                        var selecteds = $scope.config.selectedList.filter(function(item) {
+                            return item[$scope.config.pkid] == value;
+                        });
+                        var selected = selecteds.length > 0 ? selecteds[0] : null;
+                        if ($scope.checkAll && !selected) {
+                            $scope.config.selectedList.push(data);
+                        }
+                        if (!$scope.checkAll && selected) {
+                            var index = $scope.config.selectedList.indexOf(selected);
+                            $scope.config.selectedList.splice(index, 1);
+                        }
+                    });
+                    return false;
+                };
+
+                $scope.$watch("gridSource", function(newVal, oldVal) {
+                    // $scope.checkAll = false;
+                    // $scope.selectedList = [];
+                    $scope.fnCheckIsAll();
+                }, true);
+
+                $scope.fnSelectQuery = function(option, item) {
+                    item.value = option.value;
+                    item.display = option.display || option.name;
+                };
+
+                $scope.fnQuery = function() {
+                    var query = {};
+                    angular.forEach($scope.config.query.items, function(data, index, array) {
+                        query[data.key] = data.value;
+                    });
+                    $scope.config.query.action.callback(query);
+                };
+                $scope.onSort = function(value, lastSort) {
+                    if (lastSort && value.key != lastSort.key){
+                        lastSort.sort = "";
+                    }
+                    $scope.config.lastSort = value;
+                    value.sort = !value.sort||value.sort=="asc"?"desc":"asc";
+                    $scope.config.onSort(value.key + " " + value.sort, value);
+                }
+            }
+        }
+    });
---
+++ src/enuoGridCell.js
@@ -0,0 +1,60 @@
+angular.module('enuo.grid')
+    .directive('enuoGridCell', function() {
+        return {
+            restrict: 'EA',
+            replace: true,
+            transclude: true,
+            scope: {
+                column: "=",
+                item: "="
+            },
+            template: '<span ng-click="click($event)"></span>',
+            compile: function() {
+                return {
+                    pre: function($scope, $elm, $attrs) {
+                        function getFunc(func) {
+                            if (angular.isFunction(func)) {
+                                return func;
+                            }
+                            return eval("(" + func + ")");
+                        }
+
+                        var value = $scope.item[$scope.column.key];
+                        if ($scope.column.format) {
+                            value = getFunc($scope.column.format)(value, $scope.item);
+                        } else if (value === undefined) {
+                            value = $scope.column.name;
+                        } else if (value === null) {
+                            value = '';
+                        }
+
+                        if ($scope.column.template == 'add') {
+                            $elm.append("<a class='text-success text-link'><i class='glyphicon glyphicon-plus'></i> " + value + "</a>");
+                        } else if ($scope.column.template == 'edit') {
+                            $elm.append("<a class='text-primary text-link'><i class='glyphicon glyphicon-pencil'></i> " + value + "</a>");
+                        } else if ($scope.column.template == 'delete') {
+                            $elm.append("<a class='text-danger text-link'><i class='glyphicon glyphicon-remove'></i> " + value + "</a>");
+                        } else if ($scope.column.template) {
+                            $elm.append(getFunc($scope.column.template)(value, $scope.item));
+                        } else {
+                            $elm.append(value + '');
+                        }
+                    },
+                    post: function($scope, $elm, $attrs) {
+                        $scope.click = function($event) {
+                            if ($scope.column.click) {
+                                var invoke;
+                                if (angular.isFunction($scope.column.click)) {
+                                    invoke = $scope.column.click;
+                                } else {
+                                    invoke = eval("(" + $scope.column.click + ")");
+                                }
+                                invoke($scope.item[$scope.column.name], $scope.item, $event);
+                            }
+                            $event.stopPropagation();
+                        }
+                    }
+                }
+            }
+        }
+    });
